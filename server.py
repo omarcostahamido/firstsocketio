67e61d7f6d75ea59a3aa1df9534e6275d5db0ff1
Omar Costa Hamido <18335360+omarcostahamido@users.noreply.github.com>
2022-04-18 20:48:06 +0100

    last update

--- server.py
+++ server.py
@@ -1,10 +1,10 @@
 import socketio
 import eventlet
+import argparse
 
 print("Hello Fly.io!")
 
-# IP = "fly-global-services"
-IP = "127.0.0.1"
+IP = "0.0.0.0"
 PORT = 5000
 
 sio = socketio.Server(async_mode='eventlet',cors_allowed_origins='*')
@@ -18,11 +18,25 @@ def connect(sid, environ):
 def Client(sid, *data):
 	print("received message from client:",sid)
 	print("the contents of the message were:",*data)
+	sio.emit('response', data, room=sid)
 
 @sio.event
 def disconnect(sid):
 	print('disconnected from: ', sid)
 
-eventlet.wsgi.server(eventlet.listen((IP, PORT)), app)
 
-print("this should not print")
+if __name__ == '__main__':
+
+	p = argparse.ArgumentParser()
+
+	p.add_argument('port', type=int, nargs='?', default=5000, help='The port where the server.py will listen for incoming messages. Default port is 5000')
+
+	args = p.parse_args()
+
+	PORT = args.port
+
+	print("getting ready to start server at",IP,PORT)
+
+	eventlet.wsgi.server(eventlet.listen((IP, PORT)), app)
+
+	print("this should not print")
